21d2d60b667ee4f568858b96d21ba52a81bb4170
Mia Hill <119606230+miaalana@users.noreply.github.com>
2024-05-10 00:57:53 -0500

    Completed Everything

--- app.py
+++ app.py
@@ -76,7 +76,6 @@ def get_courses_for_student(student_id):
     try:
         db_connection = get_db_connection()
         mycursor = db_connection.cursor(dictionary=True)
-        
         mycursor.execute("SELECT * FROM Enrol WHERE userID = %s", (student_id,))
         courses = mycursor.fetchall()
         if courses:
@@ -213,16 +212,17 @@ def login_user():
     if not email or not password:
         return jsonify({'Error': 'Please provide correct email and password.'}), 400
     
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-    
-    cursor.execute("SELECT * FROM User WHERE email = %s AND password = %s", (email,hashed_password))
+    cursor.execute("SELECT password FROM User WHERE email = %s", (email,))
     credential_exists = cursor.fetchone()
 
-    if not credential_exists:
+    if credential_exists and bcrypt.checkpw(password.encode('utf-8'),credential_exists[0].encode('utf-8')):
+                return jsonify({'Message': 'User login successfully'}), 200
+    else:
         return jsonify({'Error': 'Enter valid credentials'}), 400
+    
+    
     cursor.close()
     conn.close()
-    return jsonify({'Message': 'User login successfully'}), 200
 
 # Should be able to retrieve all calendar events for a particular course.
 @app.route('/getCalendarEvents/<int:cid>',methods=['GET'])
@@ -318,7 +318,7 @@ def get_forums(cid):
     except Exception as e:
         return make_response({'error':str(e)},400)
 
-#
+#create forums
 @app.route('/forums/create',methods=['POST'])
 def create_forum():
     try:
@@ -377,7 +377,7 @@ def create_thread(fid):
         title = data.get('title')
         content = data.get('content')
 
-        cs.execute(f"INSERT INTO DiscussionThread (forumID, parentThreadID, title, content) VALUES ('{fid}',1,'{title}','{content}')")
+        cs.execute(f"INSERT INTO DiscussionThread (forumID, parentThreadID, title, content) VALUES ('{fid}',NULL,'{title}','{content}')")
         conn.commit()
         cs.close()
         conn.close()
@@ -458,5 +458,172 @@ def get_content(cid):
     except Exception as e:
         return make_response({'error':str(e)},400)
 
+# Endpoint for a student to submit an assignment for a course
+@app.route('/assignments/submit', methods=['POST'])
+def submit_assignment():
+    try:
+        conn = mysql.connector.connect(host = 'localhost',user = 'proj2test',password = 'proj2password123',database = 'CourseManagement')
+        cursor = conn.cursor()
+        clist = []
+        
+        data = request.json
+        cID = data.get('courseID')
+        sID = data.get('studentID')
+        submission = data.get('submission')
+
+        if not all([cID, sID, submission]):
+            return jsonify({'error': 'Missing required fields'}), 400
+
+        cursor.execute('''
+            INSERT INTO Assignment (courseID, userID, Grade,submission, submissionStatus, gradingStatus,dueDate)
+            VALUES (%s, %s,0,%s,'','','')
+        ''', (cID, sID, submission))
+        conn.commit()
+        return jsonify({'message': 'Assignment submitted successfully'}), 201
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/assignments/grade', methods=['POST'])
+def submit_grade():
+    try:
+        conn = mysql.connector.connect(host = 'localhost',user = 'proj2test',password = 'proj2password123',database = 'CourseManagement')
+        cursor = conn.cursor()
+        clist = []
+        
+        data = request.json
+        aID = data.get('assignmentID')
+        grade = data.get('grade')
+
+        if not all([aID, grade]):
+            return jsonify({'error': 'Missing required fields'}), 400
+
+        cursor.execute('''
+            UPDATE Assignment
+            SET grade = %s
+            WHERE assignmentID = %s
+        ''', (grade, aID))
+        conn.commit()
+        return jsonify({'message': ' submission successful'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+# Endpoint to retrieve all courses with 50 or more students
+@app.route('/reports/courses/50students', methods=['GET'])
+def course_50_students():
+    try:
+        conn = mysql.connector.connect(host = 'localhost',user = 'proj2test',password = 'proj2password123',database = 'CourseManagement')
+        cursor = conn.cursor()
+        clist = []
+        
+        cursor.execute('DROP VIEW IF EXISTS CourseStudentCount')
+        cursor.execute('''
+            CREATE VIEW CourseStudentCount AS SELECT c.courseID, c.courseName, COUNT(e.userID) AS num_students
+            FROM Course c
+            JOIN Enrol e ON c.courseID = e.courseID
+            GROUP BY c.courseID
+            HAVING num_students >= 50
+        ''')
+        
+        cursor.execute("SELECT * FROM CourseStudentCount")
+        courses = cursor.fetchall()
+        return jsonify({'courses': courses}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+# Endpoint to retrieve all students with 5 or more courses
+@app.route('/reports/students/5courses', methods=['GET'])
+def students_5_courses():
+    try:
+        conn = mysql.connector.connect(host = 'localhost',user = 'proj2test',password = 'proj2password123',database = 'CourseManagement')
+        cursor = conn.cursor()
+        clist = []
+        
+        cursor.execute('DROP VIEW IF EXISTS CourseStudCount')
+        cursor.execute('''
+        CREATE VIEW CourseStudCount AS SELECT e.userID, COUNT(e.courseID) AS num_courses FROM Enrol e GROUP BY e.userID HAVING num_courses >= 5
+        ''')
+        
+        cursor.execute("SELECT * FROM CourseStudCount")
+        stdts = cursor.fetchall()
+        return jsonify({'students': stdts}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+  
+ # Endpoint to retrieve all lecturers with 3 or more courses
+@app.route('/reports/lecturers/3courses', methods=['GET'])
+def get_lecturers_3_courses():
+    try:
+        conn = mysql.connector.connect(host = 'localhost',user = 'proj2test',password = 'proj2password123',database = 'CourseManagement')
+        cursor = conn.cursor()
+        clist = []
+        
+        cursor.execute('DROP VIEW IF EXISTS CourseLecturerCount')
+        cursor.execute('''
+            CREATE VIEW CourseLecturerCount AS SELECT e.userID, COUNT(e.courseID) AS num_courses
+            FROM Enrol e
+            JOIN User u ON e.userID = u.userID
+            WHERE u.role = 'lecturer'
+            GROUP BY e.userID
+            HAVING num_courses >= 3
+        ''')
+        
+        cursor.execute("SELECT * FROM CourseLecturerCount")
+        lecturers = cursor.fetchall()
+        return jsonify({'lecturers': lecturers}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+# Endpoint to retrieve the 10 most enrolled courses
+@app.route('/reports/courses/top10enrolled', methods=['GET'])
+def get_top_10_enrolled_courses():
+    try:
+        conn = mysql.connector.connect(host = 'localhost',user = 'proj2test',password = 'proj2password123',database = 'CourseManagement')
+        cursor = conn.cursor()
+        clist = []
+        
+        cursor.execute('DROP VIEW IF EXISTS MostEnrolled')
+        cursor.execute('''
+            CREATE VIEW MostEnrolled AS SELECT c.courseID, c.courseName, COUNT(e.userID) AS num_students
+            FROM Course c
+            JOIN Enrol e ON c.courseID = e.courseID
+            GROUP BY c.courseID
+            ORDER BY num_students DESC
+            LIMIT 10
+        ''')
+        
+        cursor.execute("SELECT * FROM MostEnrolled")
+        top_courses = cursor.fetchall()
+        return jsonify({'courses': top_courses}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+# Endpoint to retrieve the top 10 students with the highest overall averages
+@app.route('/reports/students/top10averages', methods=['GET'])
+def get_top_10_students_averages():
+    try:
+        conn = mysql.connector.connect(host = 'localhost',user = 'proj2test',password = 'proj2password123',database = 'CourseManagement')
+        cursor = conn.cursor()
+        clist = []
+
+        cursor.execute('DROP VIEW IF EXISTS TOPAVERAGES')
+        cursor.execute('''
+            CREATE VIEW TOPAVERAGES AS SELECT e.userID, AVG(a.Grade) AS average_grade
+            FROM Enrol e
+            JOIN Assignment a ON e.courseID = a.courseID
+            GROUP BY e.userID
+            ORDER BY average_grade DESC
+            LIMIT 10
+        ''')
+        
+        cursor.execute("SELECT * FROM TOPAVERAGES")
+        top_students = cursor.fetchall()
+
+        cursor.close()  # Close cursor after use
+        conn.close()    # Close connection after use
+
+        return jsonify({'students': top_students}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+        
 if __name__ == '__main__':
     app.run(debug=True)
